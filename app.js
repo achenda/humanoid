788abc0736bafd70cb067f8ebb89ccc831e75767
Samaune Yim <sam.aune@hotmail.com>
2017-12-11 14:43:58 +0700

    change ai

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ var builder = require('botbuilder'),
     validUrl = require('valid-url'),
     imageService = require('./image-service');
 
-
-
-// app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
-
 // Maximum number of hero cards to be returned in the carousel. If this number is greater than 10, skype throws an exception.
 var MAX_CARD_COUNT = 10;
 
@@ -35,139 +30,68 @@ app.get('/', function(req, res) {
 app.listen(process.env.port || process.env.PORT || 8080, function() {
     console.log('Achenda AI is running');
 });
-
-app.post('/api/messages', connector.listen());
 app.get('/api/param/:name', function(req, res, next) {
     res.send('hello ' + req.params.name);
     next();
 });
+app.post('/api/messages', connector.listen());
 
-// Gets the similar images by checking the type of the image (stream vs URL) and calling the appropriate image service method.
+// Bot setup
 var bot = new builder.UniversalBot(connector, function (session) {
-    if (hasImageAttachment(session)) {
-        var stream = getImageStreamFromMessage(session.message);
-        imageService
-            .getSimilarProductsFromStream(stream)
-            .then(function (visuallySimilarProducts) { handleApiResponse(session, visuallySimilarProducts); })
-            .catch(function (error) { handleErrorResponse(session, error); });
-    } else {
-        var imageUrl = parseAnchorTag(session.message.text) || (validUrl.isUri(session.message.text) ? session.message.text : null);
-        if (imageUrl) {
-            imageService
-                .getSimilarProductsFromUrl(imageUrl)
-                .then(function (visuallySimilarProducts) { handleApiResponse(session, visuallySimilarProducts); })
-                .catch(function (error) { handleErrorResponse(session, error); });
-        } else {
-            session.send('Did you upload an image? I\'m more of a visual person. Try sending me an image or an image URL');
-        }
-    }
+    var cards = getCardsAttachments();
+
+    // create reply with Carousel AttachmentLayout
+    var reply = new builder.Message(session)
+        .attachmentLayout(builder.AttachmentLayout.carousel)
+        .attachments(cards);
+
+    session.send(reply);
 });
 
-//=========================================================
-// Bots Events
-//=========================================================
+function getCardsAttachments(session) {
+    return [
+        new builder.HeroCard(session)
+            .title('Azure Storage')
+            .subtitle('Offload the heavy lifting of data center management')
+            .text('Store and help protect your data. Get durable, highly available data storage across the globe and pay only for what you use.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/storage/media/storage-introduction/storage-concepts.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/storage/', 'Learn More')
+            ]),
 
-//Sends greeting message when the bot is first added to a conversation
-bot.on('conversationUpdate', function (message) {
-    if (message.membersAdded) {
-        message.membersAdded.forEach(function (identity) {
-            if (identity.id === message.address.bot.id) {
-                var reply = new builder.Message()
-                    .address(message.address)
-                    .text('Hi! I am SimilarProducts Bot. I can find you similar products. Try sending me an image or an image URL.');
-                bot.send(reply);
-            }
-        });
-    }
-});
+        new builder.ThumbnailCard(session)
+            .title('DocumentDB')
+            .subtitle('Blazing fast, planet-scale NoSQL')
+            .text('NoSQL service for highly available, globally distributed apps—take full advantage of SQL and JavaScript over document and key-value data without the hassles of on-premises or virtual machine-based cloud database options.')
+            .images([
+                builder.CardImage.create(session, 'https://docs.microsoft.com/en-us/azure/documentdb/media/documentdb-introduction/json-database-resources1.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/documentdb/', 'Learn More')
+            ]),
 
-//=========================================================
-// Utilities
-//=========================================================
+        new builder.HeroCard(session)
+            .title('Azure Functions')
+            .subtitle('Process events with a serverless code architecture')
+            .text('An event-based serverless compute experience to accelerate your development. It can scale based on demand and you pay only for the resources you consume.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-5daae9212bb433ad0510fbfbff44121ac7c759adc284d7a43d60dbbf2358a07a/images/page/services/functions/01-develop.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/functions/', 'Learn More')
+            ]),
 
-function hasImageAttachment(session) {
-    return session.message.attachments.length > 0 &&
-        session.message.attachments[0].contentType.indexOf('image') !== -1;
-}
-
-function getImageStreamFromMessage(message) {
-    var headers = {};
-    var attachment = message.attachments[0];
-    if (checkRequiresToken(message)) {
-        // The Skype attachment URLs are secured by JwtToken,
-        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
-        // https://github.com/Microsoft/BotBuilder/issues/662
-        connector.getAccessToken(function (error, token) {
-            var tok = token;
-            headers['Authorization'] = 'Bearer ' + token;
-            headers['Content-Type'] = 'application/octet-stream';
-
-            return request.get({ url: attachment.contentUrl, headers: headers });
-        });
-    }
-
-    headers['Content-Type'] = attachment.contentType;
-    return request.get({ url: attachment.contentUrl, headers: headers });
-}
-
-function checkRequiresToken(message) {
-    return message.source === 'skype' || message.source === 'msteams';
-}
-
-/**
- * Gets the href value in an anchor element.
- * Skype transforms raw urls to html. Here we extract the href value from the url
- * @param {string} input Anchor Tag
- * @return {string} Url matched or null
- */
-function parseAnchorTag(input) {
-    var match = input.match("^<a href=\"([^\"]*)\">[^<]*</a>$");
-    if (match && match[1]) {
-        return match[1];
-    }
-
-    return null;
-}
-
-//=========================================================
-// Response Handling
-//=========================================================
-
-function handleApiResponse(session, images) {
-    if (images && images.constructor === Array && images.length > 0) {
-
-        var productCount = Math.min(MAX_CARD_COUNT, images.length);
-
-        var cards = new Array();
-        for (var i = 0; i < productCount; i++) {
-            cards.push(constructCard(session, images[i]));
-        }
-
-        // create reply with Carousel AttachmentLayout
-        var reply = new builder.Message(session)
-            .text('Here are some visually similar products I found')
-            .attachmentLayout(builder.AttachmentLayout.carousel)
-            .attachments(cards);
-        session.send(reply);
-    } else {
-        session.send('Couldn\'t find similar products images for this one');
-    }
-}
-
-function constructCard(session, image) {
-    return new builder.HeroCard(session)
-        .title(image.name)
-        .subtitle(image.hostPageDisplayUrl)
-        .images([
-            builder.CardImage.create(session, image.thumbnailUrl)
-        ])
-        .buttons([
-            builder.CardAction.openUrl(session, image.hostPageUrl, 'Buy from merchant'),
-            builder.CardAction.openUrl(session, image.webSearchUrl, 'Find more in Bing')
-        ]);
-}
-
-function handleErrorResponse(session, error) {
-    session.send('Oops! Something went wrong. Try again later.');
-    console.error(error);
+        new builder.ThumbnailCard(session)
+            .title('Cognitive Services')
+            .subtitle('Build powerful intelligence into your applications to enable natural and contextual interactions')
+            .text('Enable natural and contextual interaction with tools that augment users\' experiences using the power of machine-based intelligence. Tap into an ever-growing collection of powerful artificial intelligence algorithms for vision, speech, language, and knowledge.')
+            .images([
+                builder.CardImage.create(session, 'https://azurecomcdn.azureedge.net/cvt-68b530dac63f0ccae8466a2610289af04bdc67ee0bfbc2d5e526b8efd10af05a/images/page/services/cognitive-services/cognitive-services.png')
+            ])
+            .buttons([
+                builder.CardAction.openUrl(session, 'https://azure.microsoft.com/en-us/services/cognitive-services/', 'Learn More')
+            ])
+    ];
 }
